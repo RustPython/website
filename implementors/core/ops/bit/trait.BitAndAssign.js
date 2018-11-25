c2e48cb4bac256df3cbd3ac847a60d87d1a56917
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-25 22:34:18 +0000

    Deploy RustPython/website to github.com/RustPython/website.git:master

--- implementors/core/ops/bit/trait.BitAndAssign.js
+++ implementors/core/ops/bit/trait.BitAndAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rustpython_vm"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a> for <a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>",synthetic:false,types:["rustpython_vm::bytecode::FunctionOpArg"]},];
+implementors["rustpython_vm"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitAndAssign.html\" title=\"trait core::ops::bit::BitAndAssign\">BitAndAssign</a>&lt;<a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>&gt; for <a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>",synthetic:false,types:["rustpython_vm::bytecode::FunctionOpArg"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
