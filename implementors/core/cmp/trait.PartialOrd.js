a8b22040127c93b4135879e25c69da05f766ef4a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-25 21:18:16 +0000

    Deploy RustPython/website to github.com/RustPython/website.git:gh-pages

--- implementors/core/cmp/trait.PartialOrd.js
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rustpython_vm"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>&gt; for <a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>",synthetic:false,types:["rustpython_vm::bytecode::FunctionOpArg"]},];
+implementors["rustpython_vm"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>",synthetic:false,types:["rustpython_vm::bytecode::FunctionOpArg"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
