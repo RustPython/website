65a43217aa924eb80054ead59d07bbd744edd742
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-25 22:26:21 +0000

    Deploy RustPython/website to github.com/RustPython/website.git:master

--- aliases.js
+++ aliases.js
@@ -1,7 +1,16 @@
 var ALIASES = {};
 ALIASES['rustpython'] = {};
 ALIASES['rustpython'] = {};
+ALIASES['rustpython'] = {};
+ALIASES['rustpython'] = {};
+ALIASES['rustpython'] = {};
 ALIASES['rustpython_parser'] = {};
 ALIASES['rustpython_parser'] = {};
+ALIASES['rustpython_parser'] = {};
+ALIASES['rustpython_parser'] = {};
+ALIASES['rustpython_parser'] = {};
+ALIASES['rustpython_vm'] = {};
+ALIASES['rustpython_vm'] = {};
+ALIASES['rustpython_vm'] = {};
 ALIASES['rustpython_vm'] = {};
 ALIASES['rustpython_vm'] = {};
