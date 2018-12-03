cc00d6a587b2b5d132080ad0d15447c1e12f4faf
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-03 20:01:10 +0000

    Deploy RustPython/website to github.com/RustPython/website.git:master

--- aliases.js
+++ aliases.js
@@ -1,7 +1,4 @@
 var ALIASES = {};
 ALIASES['rustpython'] = {};
-ALIASES['rustpython'] = {};
 ALIASES['rustpython_parser'] = {};
-ALIASES['rustpython_parser'] = {};
-ALIASES['rustpython_vm'] = {};
 ALIASES['rustpython_vm'] = {};
--- implementors/core/ops/arith/trait.SubAssign.js
+++ implementors/core/ops/arith/trait.SubAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rustpython_vm"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a>&lt;<a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>&gt; for <a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>",synthetic:false,types:["rustpython_vm::bytecode::FunctionOpArg"]},];
+implementors["rustpython_vm"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a> for <a class=\"struct\" href=\"rustpython_vm/bytecode/struct.FunctionOpArg.html\" title=\"struct rustpython_vm::bytecode::FunctionOpArg\">FunctionOpArg</a>",synthetic:false,types:["rustpython_vm::bytecode::FunctionOpArg"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
